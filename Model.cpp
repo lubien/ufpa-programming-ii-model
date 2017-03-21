a67a7278db25c7d91fd87294dc780a2f9d5f59bb
lubien <lubien1996@gmail.com>
2017-03-21 16:31:42 -0300

    Add Model class

---
+++ Model.cpp
@@ -0,0 +1,43 @@
+#include "Model.h"
+using namespace std;
+
+const string Model::RESOURCE = "none";
+
+Model::Model()
+:id(-1) {}
+
+Model::Model(int id)
+:id(id) {}
+
+Model::Model(const Model &model)
+:id(model.id) {}
+
+Model::~Model() {
+	cout << "Destroying " << *this << endl;
+}
+
+// Operators
+
+ostream &operator<<(ostream &out, const Model &model) {
+	out << "Model(" << model.id << ")";
+	return out;
+}
+
+const Model &Model::operator=(const Model &right) {
+	this->id = right.id;
+	return *this;
+}
+
+bool Model::operator==(const Model &right) const {
+	return (this->id == right.id) && (this->id != -1);
+}
+
+bool Model::operator!=(const Model &right) const {
+	return !(*this == right);
+}
+
+// Public
+
+const string Model::resource() {
+	return Model::RESOURCE;
+}
---
+++ Model.h
@@ -0,0 +1,36 @@
+#ifndef _MODEL_H_
+#define _MODEL_H_
+
+#include <iostream>
+#include <ostream>
+#include <string>
+using namespace std;
+
+class Model {
+	friend ostream &operator<<(ostream &, const Model &);
+
+	public:
+
+		Model();
+		Model(int);
+		Model(const Model &);
+		~Model();
+		const Model &operator=(const Model &);
+		bool operator==(const Model &) const;
+		bool operator!=(const Model &right) const;
+
+		virtual Model* findOrFail(int) = 0;
+		virtual bool save() = 0;
+		virtual bool remove() = 0;
+		virtual bool update() = 0;
+		virtual void printJson() = 0;
+
+		const string resource();
+
+	private:
+		const static string RESOURCE;
+
+	protected:
+		int id;
+};
+#endif
