4013d982c1999c4e76e98e7a2d3f8cb423c05ce3
lubien <lubien1996@gmail.com>
2017-03-22 04:08:05 -0300

    Format main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include "Model.h"
+#include "User.h"
+#include "Todo.h"
+#include "Post.h"
 using namespace std;
 
 int main() {
-	cout << "Hello Models" << endl;
+	vector<Model*> models(3);
+	models[0] = new User;
+	models[1] = new Todo;
+	models[2] = new Post;
+
+	for (size_t i = 0; i < models.size(); i++) {
+		cout << i << endl;
+		cout << (*models[i]) << endl;
+
+		Model *derivedPtr = dynamic_cast<Model*>(models[i]);
+
+		if (derivedPtr != 0) {
+			cout << (*derivedPtr) << endl << endl;
+		}
+	}
+
+	for (size_t i = 0; i < models.size(); i++) {
+		// There's none cout since destructors print messages
+		delete models[i];
+	}
 }
